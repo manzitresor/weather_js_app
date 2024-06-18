36133726b47830804f52a35751d8d3ea2797c218
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-06-18 17:35:42 +0200

    Add styles and add some new functionality

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const apiKey = 'e44f060bfc6e19da5527b64f9251a6fd';
 const api = 'https://api.openweathermap.org/data/2.5/weather?q=kigali';
+const city = document.getElementById('city');
+const temperature = document.getElementById('temp');
+const btn = document.getElementById('search-btn');
+const search = document.getElementById('search');
+
+// btn.addEventListener('click',function(){
+//     const getcity = search.value;
+// })
 
 async function getApi (){
     const fetchApi = await fetch(api + `&appid=${apiKey}`);
@@ -7,11 +15,10 @@ async function getApi (){
     return data
 }
 
-
 async function setData() {
     let data = await getApi();
-    document.getElementById('city').innerHTML += data.name;
-    document.getElementById('temp').innerHTML += data.main.temp;
+    city.innerHTML += data.name;
+    temperature.innerHTML += data.main.temp;
 }
 
 setData();
